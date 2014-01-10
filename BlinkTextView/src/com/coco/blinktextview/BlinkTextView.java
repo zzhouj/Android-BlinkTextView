d8f5ed49bffe5829282bd578bf17217f2945f604
justin <zzhouj@yahoo.com>
2014-01-10 10:40:58 +0800

    options: fading duration, delay after fade in/out

--- BlinkTextView/src/com/coco/blinktextview/BlinkTextView.java
+++ BlinkTextView/src/com/coco/blinktextview/BlinkTextView.java
@@ -21,13 +21,21 @@ public class BlinkTextView extends TextView {
 
 	private static final int MIN_ALPHA = 0;
 	private static final int MAX_ALPHA = 255;
-	private static final int MAX_FADING_DURATION = 2000; // ms
-	private static final int MIN_FADING_DELAY = 300; // ms
-	private static final int MAX_FADING_DELAY = 800; // ms
+
+	// options constant
+	private static final int MIN_FADING_DURATION = 150; // ms
+	private static final int DEFAULT_FADING_DURATION = 2000; // ms
+	private static final int DEFAULT_DELAY_AFTER_FADEOUT = 300; // ms
+	private static final int DEFAULT_DELAY_AFTER_FADEIN = 800; // ms
 
 	// blink
 	private ColorStateList mOriginalColors;
 	private int mCurrentAlpha;
+
+	// options
+	private long mFadingDuration = DEFAULT_FADING_DURATION;
+	private long mDelayAfterFadein = DEFAULT_DELAY_AFTER_FADEIN;
+	private long mDelayAfterFadeout = DEFAULT_DELAY_AFTER_FADEOUT;
 
 	private Scroller mScroller;
 	private boolean mIsFadein;
@@ -60,6 +68,48 @@ public class BlinkTextView extends TextView {
 		final Context context = getContext();
 		mScroller = new Scroller(context, new LinearInterpolator());
 		mCurrentAlpha = MAX_ALPHA;
+	}
+
+	public long getFadingDuration() {
+		return mFadingDuration;
+	}
+
+	public void setFadingDuration(long fadingDuration) {
+		if (fadingDuration < MIN_FADING_DURATION) {
+			fadingDuration = MIN_FADING_DURATION;
+		}
+		if (mFadingDuration != fadingDuration) {
+			mFadingDuration = fadingDuration;
+			resetBlink();
+		}
+	}
+
+	public long getDelayAfterFadein() {
+		return mDelayAfterFadein;
+	}
+
+	public void setDelayAfterFadein(long delayAfterFadein) {
+		if (delayAfterFadein < 0) {
+			delayAfterFadein = 0;
+		}
+		if (mDelayAfterFadein != delayAfterFadein) {
+			mDelayAfterFadein = delayAfterFadein;
+			resetBlink();
+		}
+	}
+
+	public long getDelayAfterFadeout() {
+		return mDelayAfterFadeout;
+	}
+
+	public void setDelayAfterFadeout(long delayAfterFadeout) {
+		if (delayAfterFadeout < 0) {
+			delayAfterFadeout = 0;
+		}
+		if (mDelayAfterFadeout != delayAfterFadeout) {
+			mDelayAfterFadeout = delayAfterFadeout;
+			resetBlink();
+		}
 	}
 
 	@Override
@@ -107,6 +157,12 @@ public class BlinkTextView extends TextView {
 		mOriginalColors = getTextColors().withAlpha(mCurrentAlpha);
 	}
 
+	private void resetBlink() {
+		if (mBlinkStarted) {
+			startBlink();
+		}
+	}
+
 	private void startBlink() {
 		int sx;
 		int dx;
@@ -118,7 +174,7 @@ public class BlinkTextView extends TextView {
 			dx = -mCurrentAlpha;
 		}
 		mScroller.abortAnimation();
-		mScroller.startScroll(sx, 0, dx, 0, MAX_FADING_DURATION * Math.abs(dx) / MAX_ALPHA);
+		mScroller.startScroll(sx, 0, dx, 0, (int) (mFadingDuration * Math.abs(dx) / MAX_ALPHA));
 		ViewCompat.postInvalidateOnAnimation(this);
 		mBlinkStarted = true;
 	}
@@ -146,7 +202,7 @@ public class BlinkTextView extends TextView {
 		}
 
 		// Done with scroll, clean up state.
-		final long delayMillis = mIsFadein ? MAX_FADING_DELAY : MIN_FADING_DELAY;
+		final long delayMillis = mIsFadein ? mDelayAfterFadein : mDelayAfterFadeout;
 		getHandler().removeCallbacks(mReverseFadingRunnable);
 		getHandler().postDelayed(mReverseFadingRunnable, delayMillis);
 	}
